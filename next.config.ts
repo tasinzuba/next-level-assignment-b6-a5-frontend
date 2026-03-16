95a1d660702e532845ed374ee15bfae1cd681495
tasinzuba <tasinahmed423@gmail.com>
2026-03-17 00:57:04 +0600

    allow tmdb image domain in next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['image.tmdb.org', 'via.placeholder.com'],
+  },
 };
 
 export default nextConfig;
